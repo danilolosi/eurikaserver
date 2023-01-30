5891d4abedf58533f0b1d126bde4f81e08580301
Danilo Losi <danilo.lsanctis@hotmail.com>
2023-01-30 18:53:05 -0300

    First commit - Create Eureka Server

---
+++ src/test/java/com/danilolosi/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilolosi.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
